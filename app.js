7e9043d1c7f43f2871282110af2925986bd862e7
Macarena Perez <macamonper@gmail.com>
2021-05-21 21:01:01 -0300

    estructura js

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const monto= document.querySelector("#monto");
+const moneda = document.getElementById("moneda");
+const montoTotal = document.getElementById("montoTotal");
 
-const moneda =document.getElementById("moneda");
+moneda.addEventListener("change",()=>{
+    
+    switch (moneda.value){
+        case "euro":
+            montoTotal.textContent = 0.0087* monto.value;
+            break;
+        case "usd":
+            montoTotal.textContent =0.011* monto.value;
+            break;
+        case "mxn":
+            montoTotal.textContent= 4.73 * monto.value;
+            break;
+            case "col":
+                montoTotal.textContent= 39.78* monto.value;
 
+    }
+});
